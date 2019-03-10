0188f326d8240211f8fd2b0015a37811a0ee381e
irefuu <47826189+irefuu@users.noreply.github.com>
2019-03-10 14:16:30 +0100

    Add files via upload

--- practica2_758325_755844/c++/agrupacion-dinamica.h
+++ practica2_758325_755844/c++/agrupacion-dinamica.h
@@ -1,41 +1,51 @@
 #ifndef AGRUPACION_DINAMICA_H
 #define AGRUPACION_DINAMICA_H
 
+
 // Interfaz del TAD agrupación genérico. Pre-declaraciones:
 
 template<typename T>
 class agrupacion_dinamica{
+    friend class nodoAgrupacion;
+    class nodoAgrupacion{
+        public:
+		T dato;
+		nodoAgrupacion* sig;
+
+		nodoAgrupacion(const T& dato_, nodoAgrupacion* sig_):dato(dato_), sig(sig_){}
+		~nodoAgrupacion(){
+			delete(sig);
+		}
+	};
 	
 private: 
 	//Mantenemos aqui los mismos atributos privados, ignorando aquellos que tengan que ver
 	//con el iterador
 	int total;
-	nodoAgrupacion* inicio;
+	nodoAgrupacion* primero;
+    nodoAgrupacion* ultimo;
 
 public:
 //	Ahora la funcion iniciar de la estructura es el constructor. 
-	agrupacion_dinamica(): total(0)
+	agrupacion_dinamica()
 	{ 
-	    //TODO: Invoca a los constructores de los miembros privados (o deja
-		//que se construyan por defecto si lo consideras necesario).
-		//Separados por comas (si no sabes cómo hacerlo, mira cómo está hecho
-		//en la clase const_iterator)
-
-       	 
-		//Si invocas a los constructores de los miembros privados no necesitas rellenar el código del constructor.
+	    total = 0;
+        primero = nullptr;
+        ultimo = nullptr;
 	}
 
 //	TODO: La funcion anyadir ahora es el metodo anyadir. Rellénalo. Para acceder a atributos
 //	y métodos de la propia clase, deberás hacerlo a través del puntero this->
 	bool anyadir(const T& p)
 	{
-		typename agrupacion_dinamica<T>::nodoAgrupacion* nuevo = new nodoAgrupacion(p);
-		if(total>0){
-			nuevo->sig=inicio;
-			inicio=nuevo;
+		if(primero == nullptr){
+            nodoAgrupacion* nuevo = new nodoAgrupacion(p,nullptr);
+            primero = nuevo;
+			ultimo = nuevo;
 		}
-		else{
-			inicio=nuevo;
+		else{ //No es el primero que anyadimos
+			nodoAgrupacion* nuevo = new nodoAgrupacion(p,ultimo);
+            ultimo = nuevo;
 		}
 		total++;
 		return true;
@@ -44,28 +54,18 @@ public:
 //	TODO: La funcion borrarUltimo se transforma tambien en un metodo. Rellénalo.
 	bool borrarUltimo()
 	{
-		bool sePuede = total > 0;
+		bool sePuede = (ultimo != nullptr);
 		if(sePuede){
-			typename agrupacion_dinamica<T>::nodoAgrupacion* aux;
-			aux=inicio;
-			inicio=inicio->sig;
+			nodoAgrupacion* aux = ultimo;
+			ultimo=ultimo->sig;
 			total--;
 			delete(aux);
 		}
 		return sePuede;
 	}
 
-	friend class nodoAgrupacion;
-	class nodoAgrupacion{
-	public:
-		T dato;
-		nodoAgrupacion* sig;
-
-		nodoAgrupacion(const T& dato_) : dato(dato_),sig(nullptr){}
-		~nodoAgrupacion(){
-			delete(sig);
-		}
-	};
+	
+	
 
 	//Declarando la clase const_iterator (iterador constante) como friend y como
 	//clase local, conseguimos que pueda haber varios iteradores, cada uno de ellos
@@ -76,11 +76,11 @@ public:
 	private:
 		//Al separar el iterador en una clase aparte, trasladamos los miembros privados que estaban
 		//en el struct a esta clase. Además, mantenemos una referencia a la agrupacion.
-		int i;
 		const agrupacion_dinamica<T>& c;
+        nodoAgrupacion* iter;
 	public:
 		//Este constructor sirve como inicializador del iterador, tanto al principio como al final.
-		const_iterator(const agrupacion_dinamica& c_, int i_) : i(i_), c(c_) {  }
+		const_iterator(const agrupacion_dinamica& c_) : c(c_), iter(c.ultimo) { }
 		
 	//	En la definición por defecto de los iteradores, se separa el avance del iterador
 	//	a la obtención del elemento apuntado por el iterador (que en la otra implementación ocurren
@@ -93,7 +93,9 @@ public:
 			//TODO: Rellena este hueco para que el iterador sobre la agrupación avance. Recuerda
 			//que en nuestra definición de agrupación la estructura se recorre desde el último
 			//elemento introducido hasta el primero (como si se tratara de una pila).  
-			if(i>=0) i--;
+			if(iter->sig != nullptr){
+                iter = iter->sig;
+            } 
 			return (*this);
 	       	}
 
@@ -102,7 +104,7 @@ public:
 		const T& operator*()   const 
 		{
 			//TODO: Rellena este método para que devuelva el elemento T al que está apuntando el iterador.
-			return c.inicio->dato;	
+			return iter->dato;	
 		} 
 
 
@@ -115,7 +117,7 @@ public:
 		{ 
 			//TODO: Rellenar este método para devuelva true si este iterador y el iterador "that" apuntan
 			//a sitios diferentes, y false en caso contrario. 
-			return i != that.i;
+			return iter != that.iter;
 		}		
 
 	};
@@ -125,8 +127,8 @@ public:
 	//la comprobación de que "existe siguiente".
 	//Date cuenta que los valores que le pasamos como índice del iterador son para que se recorra la
 	//estructura desde el último elemento (this->total - 1) hasta el primero (0).
-	const_iterator begin() const { return const_iterator(*this,this->total - 1); }
-	const_iterator end()   const { return const_iterator(*this,-1); }
+	const_iterator begin() const { return const_iterator(*this); }
+	const_iterator end()   const { return const_iterator(*this); }
 };
 
 
