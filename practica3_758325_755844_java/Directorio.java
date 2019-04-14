a04929905b8caec25a4c09476cdf99e2041d8fe4
irefuu <47826189+irefuu@users.noreply.github.com>
2019-04-14 12:44:43 +0200

    Add files via upload

--- practica3_758325_755844_java/Directorio.java
+++ practica3_758325_755844_java/Directorio.java
@@ -1,3 +1,4 @@
+
 //Similar a los includes de C++, traemos aquellas clases que nos seran utiles
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
@@ -9,6 +10,25 @@ public class Directorio extends Elemento
 	public Directorio(String n){
 		nombre=n;
 		tamanyo=0;
-	}
-	
+    }
+    
+    public boolean existe(Directorio uno){
+        return contenido.contains(uno);
+    }
+    
+    public Elemento existe_name(String name){
+        int n = contenido.size();
+        int i=0;
+        boolean encontrado = false;
+        while(!encontrado && i < n){
+            Elemento nuevo = contenido.get(i);
+            if(name.equals(nuevo.getName())){
+                encontrado=true;
+            }else{
+                i++;
+            }
+        }
+        if(encontrado){ return nuevo;}
+        else{return null;}
+    }
 }
